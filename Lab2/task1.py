38f424ea97b79ff5347d41a915f32e40270284f9
bespalovad <55587308+bespalovad@users.noreply.github.com>
2019-11-12 17:43:04 +0300

    Create task1.py

---
+++ Lab2/task1.py
@@ -0,0 +1,26 @@
+import re
+
+def int_valid (prompt):
+	a = input (prompt)
+	while not bool (re.match(r'[0-9](\d+)?\Z', a)):
+		print ("Необхідно ввести натуральне число")
+		a = input (prompt)
+	return (int(a))
+	
+def float_valid (prompt):
+	a = input (prompt)
+	while not bool (re.match(r'(?:[0]\.\d+)|(?:[1-9](?:\d+)?(?:\.\d+)?)\Z', a)):
+		print ("Необхідно ввести дійсне число")
+		a = input (prompt)
+	return (float(a))
+
+print("Корнієнко Т.С.\n Лабораторна робота 2\n Варіант13\n Організація циклу за допомогою оператора for\n ")
+
+
+x = float_valid("Введіть значення x ")
+s = 0
+n = int_valid("Введіть значення n ")
+
+for i in range(n):
+    s = s + (x+i)**2
+print(s)
